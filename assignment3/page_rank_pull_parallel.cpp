b3b85368b338d5812dfd39397f1bca95d7e56479
eugenesujing <eugenesujing@gmail.com>
2021-06-15 17:11:25 -0700

    f

--- assignment3/page_rank_pull_parallel.cpp
+++ assignment3/page_rank_pull_parallel.cpp
@@ -36,7 +36,7 @@ void* pageRankParallel(void* arg){
   timer t1;
   uintV n = args->g->n_;
   int interval = n/args->n_threads;
-  if(args->tid==n->args->n_threads-1){
+  if(args->tid==args->n_threads-1){
     interval += n/args->n_threads;
   }
   t1.start();
