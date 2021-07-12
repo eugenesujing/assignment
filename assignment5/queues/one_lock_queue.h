ac7df8c044dfc7bc077f8086cf9435ef37f4e1ca
eugenesujing <eugenesujing@gmail.com>
2021-07-12 14:55:49 -0700

    one lock complete

--- assignment5/queues/one_lock_queue.h
+++ assignment5/queues/one_lock_queue.h
@@ -1,13 +1,20 @@
 #include "../common/allocator.h"
+#include <pthread.h>
+
+pthread_mutex_t* locks = new pthread_mutex_t[n];
 
 template <class T>
 class Node
 {
+  T value;
+  Node<T>* next;
 };
 
 template <class T>
 class OneLockQueue
 {
+    Node<T>* q_head;
+    Node<T>* q_tail;
     CustomAllocator my_allocator_;
 public:
     OneLockQueue() : my_allocator_()
@@ -20,16 +27,36 @@ public:
         my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
         // Initialize the queue head or tail here
         Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        my_allocator_.freeNode(newNode);
+        newNode->next = NULL;
+        q_head = newNode;
+        q_tail = newNode;
+
     }
 
     void enqueue(T value)
     {
+      node = (Node<T>* )my_allocator.newNode();
+      node->value = value;
+      node->next = NULL;
+      //Append to q_tail and update the queue
+      pthread_mutex_lock(locks);
+      q_tail->next = node;
+      q_tail = node;
+      pthread_mutex_unlock(locks);
     }
 
     bool dequeue(T *value)
     {
         bool ret_value = false;
+        pthread_mutex_lock(locks);
+        if(q_head->next!=NULL){
+          Node<T>* node = q_head;
+          *value = q_head->next->value;
+          q_head = q_head->next;
+          my_allocator.freeNode(node);
+          ret_value = true;
+        }
+        pthread_mutex_unlock(locks);
         return ret_value;
     }
 
