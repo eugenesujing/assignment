1d8ba020cac1c79ed6f854794a00ce580ee11d88
eugenesujing <eugenesujing@gmail.com>
2021-07-13 00:33:11 -0700

    14

--- assignment5/queues/non_blocking_queue.h
+++ assignment5/queues/non_blocking_queue.h
@@ -73,6 +73,7 @@ public:
             LFENCE;
             if (&tail == &q_tail){
                 if (next.address() == NULL) {
+                  std::cout<<"enque here 1"<<std::end;
                   Node<T>* p = compute_ptr(node,next.count()+1);
                   pointer_t<Node<T>> to_be_swapped ={p};
                     if(CAS(&tail.address()->next, next, to_be_swapped)){
@@ -82,6 +83,7 @@ public:
 
                 }
                 else{
+                  std::cout<<"enque here 2"<<std::end;
                   Node<T>* p = compute_ptr(next.address(),tail.count()+1);
                   pointer_t<Node<T>> to_be_swapped ={p};
                     CAS(&q_tail, tail, to_be_swapped);	// ELABEL
@@ -107,6 +109,7 @@ public:
              LFENCE;
              if (&head == &q_head) {
                  if(head.address() == tail.address()) {
+                   std::cout<<"deque here 1"<<std::end;
                      if(next.address() == NULL)
                              return false;
                     Node<T>* p = compute_ptr(next.address(),tail.count()+1);
@@ -114,6 +117,7 @@ public:
                      CAS(&q_tail, tail, to_be_swapped);	//DLABEL
                  }
                  else {
+                   std::cout<<"deque here 1"<<std::end;
                      *value = next.address()->value;
                      Node<T>* p = compute_ptr(next.address(),head.count()+1);
                      pointer_t<Node<T>> to_be_swapped ={p};
